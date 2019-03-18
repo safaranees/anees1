12324b176fbf633b00f18f606a2e3b0ee2353aa3
safaranees <aneesparvin08@gmail.com>
2019-03-18 11:44:12 +0530

    Create OddNumber.java

---
+++ OddNumber.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class OddNumber
+{
+  public static void main(String args[])
+  {
+    Scanner num=new Scanner(System.in);
+    int a=num.nextInt();
+    int b=num.nextInt();
+    int c=a+1;
+    int d=b-1;
+    for(int i=c;i<=d;i++)
+    {
+      if(i%2!=0)
+      {
+        System.out.println(i);
+      }
+    }
+  }
